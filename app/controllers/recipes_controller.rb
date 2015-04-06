fe03e2f5de05170d86cfc459e22a292361939e48
Jakson Rochelly <jrochelly@gmail.com>
2015-04-06 10:06:03 -0300

    Add authentication and layout structure

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :set_recipe, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:show, :index]
 
   # GET /recipes
   # GET /recipes.json
